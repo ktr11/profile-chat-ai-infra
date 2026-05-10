e469a172d7320e4b3ca5e646b2e8476af8f77f1d
ktr11 <54854477+ktr11@users.noreply.github.com>
2026-05-10 11:18:43 +0900

    db定義確認してコミット

--- lib/stage.ts
+++ lib/stage.ts
@@ -7,7 +7,7 @@ export class AppStage extends cdk.Stage {
   constructor(scope: Construct, id: string, props?: cdk.StageProps) {
     super(scope, id, props);
 
-    new StorageStack(this, 'Storage', props);
+    new StorageStack(this, 'Storage', { ...props, stageName: id as 'dev' | 'prod' });
     new IamStack(this, 'Iam', props);
   }
 }
